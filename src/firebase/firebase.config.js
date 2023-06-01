c2ab55bf4bb050c9eef0cf9af506c655b75efd76
Muhammad Yahya <zulfikeryahya@gmail.com>
2023-06-02 01:25:44 +0600

    hide firebase config

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,13 +1,12 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-
 const firebaseConfig = {
-  apiKey: "AIzaSyA5HWwyxMACGPpU3YDPSY4VJkfrZwHhP_Y",
-  authDomain: "the-dragon-news-75445.firebaseapp.com",
-  projectId: "the-dragon-news-75445",
-  storageBucket: "the-dragon-news-75445.appspot.com",
-  messagingSenderId: "473704133451",
-  appId: "1:473704133451:web:8c553c47f0fa6d50666ece"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
